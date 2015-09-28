bfce6771de79dca8ea450b1f3d0f602e65db2def
clayton easterbrooks & yelena nedelko <student@epicodus.com>
2015-09-28 15:06:35 -0700

    add component + form for new listings

--- app/components/add-listing.js
+++ app/components/add-listing.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addListing() {
+      var params = {
+        title: this.get('title'),
+        owner: this.get('owner'),
+        body: this.get('body'),
+      }
+    }
+  }
+
 });
